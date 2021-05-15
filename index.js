80561133098b49de587830014549ac4980ae7870
Valentin <valentindemchyk88@gmail.com>
2021-05-15 22:06:39 +0300

    color-switch

---
+++ index.js
@@ -0,0 +1,39 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+    body: document.body,
+}
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+let timerId = null;
+
+function onStart() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+    }, 1000);
+    refs.startBtn.desabled = true;
+}
+
+function onStop() {
+    clearInterval(timerId);
+    refs.startBtn.desabled = false;
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+
+
